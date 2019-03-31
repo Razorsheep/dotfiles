23ec8bef372054198719b6f7913a467de994069d
Phillip Raffnsoe <phillip@praffn.dk>
2019-03-31 16:42:55 +0200

    update install script to make it prettier

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-#inspiration https://github.com/adityarpillai/jumpstart/blob/master/jumpstart
+
+bold=$(tput bold)
+green=$(tput setaf 76)
+reset=$(tput sgr0)
+grey=$(tput setaf 239)
+
+put_header() {
+  printf "\n${bold}===== %s =====${reset}\n" "$@"
+}
+
+put_step() {
+  printf "➜ %s\n" "$@"
+}
+
+put_info() {
+  printf "${grey}! %s\n${reset}" "$@"
+}
+
+put_success() {
+  printf "${green}✔ %s${reset}\n" "$@"
+}
 
 set -e
 
@@ -12,6 +32,8 @@ echo -e "\033[38;5;255m\033[48;5;234m\033[1m                         \033[0m\n"
 
 read -p "This script will install a lot of stuff, and change your settings. Press [enter] to continue..."; echo;
 
+put_header "Symbolic Links"
+
 # creates symbol link $1 -> $2 iff $2 doesnt exists
 ln_if () {
   if [ ! -f "$2" ]; then
@@ -21,7 +43,7 @@ ln_if () {
 
 # symlinks
 
-echo "Setting up symbolic links"; echo;
+put_step "Setting up symbolic links"
 ln_if $HOME/.dotfiles/git/gitignore_global $HOME/.gitignore_global
 ln_if $HOME/.dotfiles/zsh/zsh-aliases $HOME/.zsh-aliases
 ln_if $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
@@ -31,70 +53,93 @@ mkdir -p "$HOME/Library/Application Support/Code/User"
 ln_if $HOME/.dotfiles/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
 ln_if $HOME/.dotfiles/vscode/keybindings.json "$HOME/Library/Application Support/Code/User/keybindings.json"
 
+put_success "Symlinks created"
 
 # Install Xcode Command Line tools
+put_header "XCode Command Line Tools"
+
 if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
   test -d "${xpath}" && test -x "${xpath}" ; then
-  echo "Xcode Command Line Tools are already installed..."; echo;
+  put_info "Xcode Command Line Tools are already installed...";
 else
-  echo "Installing Xcode Command Line Tools..."; echo;
+  put_step "Installing Xcode Command Line Tools..."; echo;
   xcode-select --install
 fi
 
+put_success "Xcode Command Line tools installed"
+
 # Install Homebrew
+put_header "Homebrew"
+
 if test ! $(which brew); then
-  echo "Installing Homebrew";
+  put_step "Installing Homebrew..."
   yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /dev/null
 else
-  echo "Homebrew is already installed...";
+  put_info "Homebrew is already installed...";
 fi
 
-echo "Updating and upgrading Homebrew...";
+put_step "Updating and upgrading Homebrew...";
 yes | brew update &> /dev/null
 yes | brew upgrade &> /dev/null
 
-echo "Installing Homebrew packages..."; echo;
-yes | brew bundle --file=brewfile &> /dev/null
+put_step "Installing Homebrew packages...";
+yes | brew bundle --file=brewfile &> /dev/null || true
+
+put_success "Homebrew installed, updated and upgraded"
 
 # ZSH and oh-my-sh
+put_header "ZSH and oh-my-zsh"
 
 # Install oh-my-zsh if it doesnt exists
 if [ -d "$HOME/.oh-my-zsh" ]; then
-  echo "oh-my-zsh is already installed..."
+  put_info "oh-my-zsh is already installed..."
 else
-  echo "Installing oh-my-zsh..."
+  put_step "Installing oh-my-zsh..."
   git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh &> /dev/null
 fi
 
 # change shell to zsh if it isnt already
 TEST_CURRENT_SHELL=$(basename "$SHELL")
 if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-  echo "Changing shell to zsh"
+  put_step "Changing shell to zsh"
   chsh -s $(grep /zsh$ /etc/shells | tail -1)
 else
-  echo "Shell is already zsh..."
+  put_info "Shell is already zsh..."
 fi
 
-echo;
+put_success "oh-my-zsh installed and shell changed to zsh"
 
 
 # nvm
+put_header "NVM"
 
 # Install nvm if it doesnt exists
 if [ -d "$HOME/.nvm" ]; then
-  echo "NVM already installed...";
+  put_info "NVM already installed...";
 else
-  echo "Installing NVM...";
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+  put_step "Installing NVM...";
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash &> /dev/null
 fi
 
 source ~/.nvm/nvm.sh
-echo "Installing latest Node LTS via NVM..."; echo;
+put_step "Installing latest Node LTS via NVM..."
 nvm install --lts &> /dev/null
 
-# macos stuff
-echo "Updating macOS preferences and settings..."; echo;
-source .macos
+put_success "NVM and latest Node LTS has been installed"
 
-echo -e "\033[1;92mDone!\033[0m"
-echo "Close and open your terminal again, or source \$HOME/.zshrc"; echo;
+# macos stuff
+put_header "macOS"
+put_step "Updating macOS preferences and settings..."; echo;
+source .macos
+put_success "macOS settings updated"
+
+printf "${bold}
+            .     '     ,
+               ${green}D O N E${reset}${bold}
+              _________
+           _ /_|_____|_\ _
+             '. \   / .'
+               '.\ /.'
+                 '.'${reset}
+\n"
+put_info "Close and open your terminal again, or source \$HOME/.zshrc"
